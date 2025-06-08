9988fb67abd5d447b373e9881919d6b18dcb41f7
Sila Pakdeewong <silapakdeewong2546.3@gmail.com>
2025-06-09 00:56:43 +0700

    feat(api): add endpoint to retrieve student skills and CLO results; implement helper functions for course skills and grading data retrieval

--- apps/api/src/routes/students.route.ts
+++ apps/api/src/routes/students.route.ts
@@ -1,7 +1,7 @@
-import Elysia, { t } from "elysia";
+import Elysia, { error, t } from "elysia";
 import { authPlugin } from "../middlewares/auth";
 import { errorResponseSchema } from "../schemas/response.schema";
-import { prisma } from "../config/prisma";
+import { Clo, GradingCloResult, prisma } from "../config/prisma";
 import { baseResponseSchema } from "./response";
 import { StudentResponse, studentResponseSchema } from "../schemas/student.schema";
 import { ERROR_RESPONSES } from "../error";
@@ -134,4 +134,319 @@ export const studentsRoutes = new Elysia({
         }),
       },
     }
+  )
+
+  .get(
+    "/:studentId/skills",
+    async ({ params, error }) => {
+      try {
+        const { studentId } = params;
+
+        const coursesAndCloResults = await getStudentCoursesAndCloResults(studentId);
+
+        return {
+          statusCode: 200,
+          isSuccess: true,
+          data: coursesAndCloResults,
+          error: null,
+        };
+      } catch (err) {
+        return error(500, {
+          statusCode: 500,
+          isSuccess: false,
+          error: {
+            message: err instanceof Error ? err.message : "Internal Server Error",
+          },
+          data: null,
+        });
+      }
+    },
+    {
+      response: {
+        200: t.Object({
+          ...baseResponseSchema,
+          data: t.Any(),
+        }),
+      },
+    }
   );
+
+async function getCourseSkillsLevelsCriterias(courseId: string) {
+  return await prisma.course.findUnique({
+    where: {
+      id: courseId,
+      isDeleted: false,
+    },
+    include: {
+      courseSkills: {
+        where: {
+          courseId: courseId,
+          isDeleted: false,
+        },
+        include: {
+          skill: {
+            include: {
+              skillLevels: {
+                where: {
+                  isDeleted: false,
+                },
+                include: {
+                  methods: {
+                    where: {
+                      isDeleted: false,
+                    },
+                  },
+                  criterias: {
+                    where: {
+                      isDeleted: false,
+                    },
+                    include: {
+                      cloSkillLevelCriterias: {
+                        where: {
+                          courseId: courseId,
+                          isDeleted: false,
+                        },
+                        include: {
+                          clo: true,
+                        },
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  });
+}
+
+async function getStudentCourseGradingAndCloResults(studentId: string, courseId: string) {
+  return prisma.studentCourseGrading.findFirst({
+    where: {
+      studentCourse: {
+        studentId: studentId,
+        courseId: courseId,
+        isDeleted: false,
+      },
+    },
+    include: {
+      gradingCloResults: true,
+    },
+  });
+}
+
+type LevelAndCriteria = {
+  level: number;
+  criterias: {
+    id: string;
+    skillLevelId: string;
+    criteriaNameTh: string;
+    criteriaNameEn: string;
+    isDeleted: boolean;
+    deletedAt: Date | null;
+    cloSkillLevelCriterias: {
+      id: string;
+      courseId: string;
+      clo: Clo;
+      cloId: string;
+      skillLevelCriteriaId: string;
+    }[];
+  }[];
+};
+
+type LevelAndCriteriaWithPassingStatus = {
+  level: number;
+  isPass: boolean;
+  criterias: {
+    id: string;
+    skillLevelId: string;
+    criteriaNameTh: string;
+    criteriaNameEn: string;
+    isDeleted: boolean;
+    deletedAt: Date | null;
+    cloSkillLevelCriterias: {
+      id: string;
+      courseId: string;
+      clo: Clo;
+      cloId: string;
+      skillLevelCriteriaId: string;
+      isPass: boolean;
+    }[];
+  }[];
+};
+
+// function getLevelsAndCriteriasPassingStatus(
+//   levelAndCriterias: LevelAndCriteria,
+//   gradingCloResults: GradingCloResult[]
+// ): LevelAndCriteriaWithPassingStatus {
+//   const result = {
+//     level: levelAndCriterias.level,
+//     isPass: false,
+//     criterias: levelAndCriterias.criterias.map((criteria) => {
+//       const cloIds = criteria.cloSkillLevelCriterias;
+//       const cloResults = gradingCloResults.filter((gcr) =>
+//         cloIds.some((clo) => clo.cloId === gcr.cloId)
+//       );
+
+//       return {
+//         id: criteria.id,
+//         skillLevelId: criteria.skillLevelId,
+//         criteriaNameTh: criteria.criteriaNameTh,
+//         criteriaNameEn: criteria.criteriaNameEn,
+//         isDeleted: criteria.isDeleted,
+//         deletedAt: criteria.deletedAt,
+//         cloSkillLevelCriterias: criteria.cloSkillLevelCriterias.map((clo) => {
+//           const cloResult = cloResults.find((gcr) => gcr.cloId === clo.cloId);
+//           return {
+//             ...clo,
+//             isPass: cloResult ? cloResult.result !== "X" && cloResult.result !== "FAIL" : false,
+//           };
+//         }),
+//       };
+//     }),
+//   };
+
+//   result.isPass = result.criterias.every((criteria) =>
+//     criteria.cloSkillLevelCriterias.every((clo) => clo.isPass)
+//   );
+
+//   return result;
+// }
+
+function getLevelsAndCriteriasPassingStatus(
+  levelAndCriterias: LevelAndCriteria,
+  gradingCloResults: GradingCloResult[]
+): LevelAndCriteriaWithPassingStatus {
+  const cloResultMap = new Map(gradingCloResults.map((gcr) => [gcr.cloId, gcr]));
+
+  const criteriasWithStatus = levelAndCriterias.criterias.map((criteria) => {
+    const cloSkillLevelCriteriasWithStatus = criteria.cloSkillLevelCriterias.map((clo) => {
+      const cloResult = cloResultMap.get(clo.cloId);
+      return {
+        ...clo,
+        isPass: cloResult ? cloResult.result !== "X" && cloResult.result !== "FAIL" : false,
+      };
+    });
+
+    return {
+      ...criteria,
+      cloSkillLevelCriterias: cloSkillLevelCriteriasWithStatus,
+    };
+  });
+
+  const isPass = criteriasWithStatus.every((criteria) =>
+    criteria.cloSkillLevelCriterias.every((clo) => clo.isPass)
+  );
+
+  return {
+    level: levelAndCriterias.level,
+    isPass,
+    criterias: criteriasWithStatus,
+  };
+}
+
+async function getStudentCoursesAndCloResults(studentId: string) {
+  const studentCourses = await prisma.studentCourse.findMany({
+    where: {
+      studentId,
+      isDeleted: false,
+    },
+  });
+
+  const coursesAndCloResults = [];
+
+  for (const studentCourse of studentCourses) {
+    const courseId = studentCourse.courseId;
+
+    const courseSkillsLevelsCriterias = getCourseSkillsLevelsCriterias(courseId);
+    const studentCourseGradingAndCloResults = getStudentCourseGradingAndCloResults(
+      studentId,
+      courseId
+    );
+
+    const [cslc, sclc] = await Promise.all([
+      courseSkillsLevelsCriterias,
+      studentCourseGradingAndCloResults,
+    ]);
+
+    if (!cslc || !sclc) {
+      return error(404, ERROR_RESPONSES.notFound);
+    }
+
+    const result = {
+      id: cslc.id,
+      skillMappingRefId: cslc.skillMappingRefId,
+      courseCode: cslc.courseCode,
+      nameTh: cslc.nameTh,
+      nameEn: cslc.nameEn,
+      descriptionTh: cslc.descriptionTh,
+      descriptionEn: cslc.descriptionEn,
+      curriculumId: cslc.curriculumId,
+      isDeleted: cslc.isDeleted,
+      deletedAt: cslc.deletedAt,
+      courseSkills: cslc.courseSkills.map((cs) => ({
+        skillMappingRefId: cs.skill.skillMappingRefId,
+        nameTh: cs.skill.nameTh,
+        nameEn: cs.skill.nameEn,
+        descriptionTh: cs.skill.descriptionTh,
+        descriptionEn: cs.skill.descriptionEn,
+        type: cs.skill.type,
+        isMainSkill: cs.skill.isMainSkill,
+        studentLevel: 0, // This will be calculated later
+        id: cs.id,
+        skillId: cs.skillId,
+        skillLevels: cs.skill.skillLevels.map((sl) => ({
+          id: sl.id,
+          skillId: sl.skillId,
+          ...getLevelsAndCriteriasPassingStatus(
+            {
+              level: sl.level,
+              criterias: sl.criterias.map((c) => ({
+                id: c.id,
+                skillLevelId: c.skillLevelId,
+                criteriaNameTh: c.criteriaNameTh,
+                criteriaNameEn: c.criteriaNameEn,
+                isDeleted: c.isDeleted,
+                deletedAt: c.deletedAt,
+                cloSkillLevelCriterias: c.cloSkillLevelCriterias.map((clc) => ({
+                  id: clc.id,
+                  courseId: clc.courseId,
+                  clo: clc.clo,
+                  cloId: clc.cloId,
+                  skillLevelCriteriaId: clc.skillLevelCriteriaId,
+                })),
+              })),
+            },
+            sclc.gradingCloResults
+          ),
+          descriptionTh: sl.descriptionTh,
+          descriptionEn: sl.descriptionEn,
+          methods: sl.methods.map((m) => ({
+            id: m.id,
+            skillLevelId: m.skillLevelId,
+            methodNameTh: m.methodNameTh,
+            methodNameEn: m.methodNameEn,
+            isDeleted: m.isDeleted,
+            deletedAt: m.deletedAt,
+          })),
+          isDeleted: sl.isDeleted,
+          deletedAt: sl.deletedAt,
+        })),
+      })),
+    };
+
+    for (const courseSkill of result.courseSkills) {
+      let maxLevel = 0;
+      for (const level of courseSkill.skillLevels) {
+        if (!level.isPass) break;
+        maxLevel = level.level;
+      }
+      courseSkill.studentLevel = maxLevel;
+    }
+
+    coursesAndCloResults.push(result);
+  }
+  return coursesAndCloResults;
+}
